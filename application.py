206bcc729a8fff98db63a77b7e8800b1b88702ff
HarshStats <harshgrana97@gmail.com>
2025-06-10 15:20:24 +0200

    port changed

--- application.py
+++ application.py
@@ -44,5 +44,5 @@ def predict_datapoint():
     return render_template('home.html', result=None)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
 
